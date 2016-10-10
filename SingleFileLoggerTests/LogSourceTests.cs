f772d3bdd07b1f2781a4c0f16575e1c00cdf65db
68681395 <happycoder@foxmail.com>
2016-10-10 16:11:18 +0800

    initial

---
+++ SingleFileLoggerTests/LogSourceTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tsharp
+{
+    [TestClass()]
+    public class LogSourceTests
+    {
+        [TestMethod()]
+        public void LogSourceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LogSourceTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WriteLineTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ SingleFileLoggerTests/RollingFlatFileTraceListenerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tsharp
+{
+    [TestClass]
+    public class RollingFlatFileTraceListenerTests
+    {
+        [TestMethod]
+        public void RollingFlatFileTraceListenerTest()
+        {
+
+
+            var writer = new SimpleFileLogger.RollingFlatFileTraceListener(
+                "trace.log",
+                null,
+                null,
+                512,
+                "HHmmss",
+                "yyyyMMdd",
+                SimpleFileLogger.RollFileExistsBehavior.Increment,
+                SimpleFileLogger.RollInterval.Day);
+
+            writer.WriteLine("rrrrrr");
+
+            writer.Flush();
+        }
+      
+
+        [TestMethod( )]
+        public void WriteCsvLineTest()
+        {
+            var rsg = new RandomStringGenerator(true, true, true, true);
+
+            using (
+                var writer = new SimpleFileLogger.RollingFlatFileTraceListener(
+                    "trace.log",
+                    null,
+                    null,
+                    512,
+                    "HHmmss",
+                    "yyyyMMdd",
+                    SimpleFileLogger.RollFileExistsBehavior.Increment,
+                    SimpleFileLogger.RollInterval.Day))
+            {
+                foreach (var i in Enumerable.Range(1, 3)) writer.WriteCsvLine(Enumerable.Range(1, 8).Select(x => rsg.Generate(3, 6)).ToArray());
+                foreach (var i in Enumerable.Range(1, 3))
+                    writer.WriteCsvLine(
+                        Enumerable.Range(1, 8)
+                            .Select(x => rsg.Generate(2) + Environment.NewLine + rsg.Generate(1, 2))
+                            .ToArray());
+                writer.Flush();
+            }
+        }
+    }
+}
